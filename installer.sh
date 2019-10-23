01382243d6b69c1c5fbea9e3e4bb4e1b0a3cc43c
shutdown57 <alinkokomansuby@gmail.com>
2019-10-24 01:04:30 +0700

    edit database
    edit database

--- installer.sh
+++ installer.sh
@@ -29,7 +29,8 @@ echo -e $b" |_| |_|_|/ |\__,_|___\__, |_| |_|"
 echo -e $b"        |__/          |___/       "
 echo -e $k" +--------------------------------+"
 echo -e $k" |   HijaIyh Premium PHP Redirect |"
-echo -e $k" |  @author  :: justalinko        |"
+echo -e $h" |    Installer Script - v1.0     |"$n
+echo -e $m" |    @author  :: justalinko      |"$n
 echo -e $k" +--------------------------------+"$n
 
 }
@@ -50,45 +51,65 @@ iyh_depen()
 		exit 1
 	fi
 
-	clear
+	clear ; iyh_banner
 	echo "Lets start bruhhh ...."
 	sleep 1
 	echo "[!] Updating your system ...."
 	apt-get update -y
-	clear
+	clear ; iyh_banner
 	echo "[!] Installing apache2 webserver , php ..."
 	apt-get install apache2 libapache2-mod-php php php-cli php-curl php-zip php-common php-mbstring php-mysql -y
-	clear
+	clear ; iyh_banner
     echo "[!] Installing mariadb-server ..."
     apt-get install mariadb-server -y
-    clear
+    clear ; iyh_banner
 	echo "[!] Installing unzip,wget,git ..."
 	apt-get install unzip wget git -y
-	clear
+	clear ; iyh_banner
 	echo "Installing dependencies DONE."
 	echo "Press ENTER to continue"; read x
 }
 iyh_install()
 {
 
-	clear
+	clear ; iyh_banner
 	echo "[!] Cloning from repository ..."
 	git clone https://github.com/justalinko/hijaiyh-ppr.git
 	echo "[!] Moving to /var/www/html ..."
 	mv hijaiyh-ppr/* /var/www/html/
+	mv hijaiyh-ppr/.htaccess /var/www/html/
 	sleep 1
 	echo "[!] Configuring databases ..."
     echo "!!!! Create database password !!!!!"
     mysql_secure_installation
-    clear
+    clear ; iyh_banner
     read -p "Input database password : " dbpass
+    echo "[!] Cleaning database ..."
+    mysql -h localhost -u root -p$dbpass -e "grant all privileges on *.* to 'root'@'localhost' identified by '${dbpass}'; "
+    mysql -h localhost -u root -p$dbpass -e 'flush privileges;'
     echo "[!] Creating database ..."
     mysql -h localhost -u root -p$dbpass -e 'CREATE DATABASE hijaiyh_ppr;'
     echo "[!] Importing database ..."
-    mysql -h localhost -u root -p$dbpass hijaiyh_ppr -e 'source hijaiyh_ppr/hijaiyh_ppr.sql;'
-    echo "[!] Cleaning database ..."
-    mysql -h localhost -u root -p$dbpass -e 'GRANT ALL PRIVILEGES ON *.* to "root"@"localhost" identified by "$dbpass"; '
-    mysql -h localhost -u root -p$dbpass -e 'FLUSH PRIVILEGES;'
+    mysql -h localhost -u root -p$dbpass hijaiyh_ppr -e 'source /var/www/html/hijaiyh_ppr.sql;'
+    sleep 2
+    echo "[!] Configuring database.php ..."
+    sed -i "s|#hijaiyh_password#|${dbpass}|g" /var/www/html/_AppData/config/database.php
+    sleep 1
+    cat /var/www/html/_AppData/config/database.php
+    echo "Ensure this config was correct."
+    echo "if wrong you can edit /var/www/html/_AppData/config/database.php ! "
+    read -p "[PRESS ENTER TO CONTINUE]" x
+
+    clear ; iyh_banner
+    echo "[!] Activating mod rewrite ..."
+    a2enmod rewrite
+    echo "[!] Replacing apache2 config ..."
+    wget -q https://pastebin.com/raw/3JHuq3Wm -O apache2.conf
+    rm /etc/apache2/apache2.conf
+    mv apache2.conf /etc/apache2/
+    sleep 1
+    echo "[!] Restart apache2 ///"
+    /etc/init.d/apache2 restart
 
 
 }
